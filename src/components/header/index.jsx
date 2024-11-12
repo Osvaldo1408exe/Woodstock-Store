718fd6a278d33940d7a5b30583bf7f45ba3cce55
Osvaldo1408exe <osvaldo1408.exe@gmail.com>
2024-11-12 00:38:35 -0300

    feat: header adicionado

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,28 +1,40 @@
-import "./style.css"
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import './style.css';
+
 export function Header() {
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    const alternarMenu = () => {
+        setMenuAberto(!menuAberto);
+    };
+
     return (
         <header>
             <div className="container">
                 <div className="logoContainer">
-                    <img src="/images/logo.png" alt="logo" className="logo"/>
+                    <img src="public/logo.svg" alt="logo" className="logo" />
                     <h1>Woodstock Store</h1>
                 </div>
-                <div className="pages-section">
+
+                 <div className="menu-icon" onClick={alternarMenu}>
+                    <FontAwesomeIcon icon={menuAberto ? faTimes : faBars} />
+                </div>
+
+                 <div className={menuAberto ? 'dropdown-menu active' : 'dropdown-menu'}>
                     <ul className="list-pages">
                         <li><a href="#">Home</a></li>
                         <li><a href="#">Products</a></li>
                         <li><a href="#">About</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
-                </div>
-                <div className="user-section">
                     <ul className="list-user">
                         <li><a href="#">Login</a></li>
                         <li><a href="#">Register</a></li>
                     </ul>
                 </div>
-                
             </div>
         </header>
-    )
+    );
 }
