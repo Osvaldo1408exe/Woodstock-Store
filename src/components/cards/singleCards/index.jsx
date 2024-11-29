8b973358c4b682af128af35235728fd069b4abb4
Osvaldo1408exe <osvaldo1408.exe@gmail.com>
2024-11-29 04:04:20 -0300

    feat: cartões adicionados

--- src/components/cards/singleCards/index.jsx
+++ src/components/cards/singleCards/index.jsx
@@ -1,9 +1,14 @@
 import Styles from'./Component.module.css'
-export default function GenreCards() {
+
+export default function SingleCards(props) {
     return (
         <>
-            
-        
+        <div className={Styles.card} key={props.id}>
+            <img src={props.photoPath} alt={props.album} />
+            <p className={Styles.album}>{props.album}</p>
+            <p className={Styles.band}>{props.band}</p>
+            <p className={Styles.price}>${props.price}</p>
+        </div>
         </>
     )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,44 @@
-import MainBanner from "../components/banners/mainBanner";
+ import MainBanner from "../components/banners/mainBanner";
 import HomeCard from "../components/cards/homeCard";
+import SingleCards from "../components/cards/singleCards";
 import Footer from "../components/footer";
 import Header from "../components/header";
 
+ 
+import { useState, useEffect } from 'react';
+
 export default function Home() {
+    const [albums, setAlbums] = useState([]);
+
+    
+    useEffect(() => {
+        fetch('./products.json')  
+            .then((response) => response.json())
+            .then((data) => setAlbums(data));
+    }, []); 
+
     return (
         <>
             <Header/>
-            <MainBanner/>
-            <HomeCard/>
-            <Footer/>
+            <MainBanner />
+            <HomeCard />
+
+            <div style={{ textAlign: "center", fontSize: "16px", fontWeight: "bold", marginBottom: "40px" }}>
+                <h2>Some of our Products</h2>
+                <div className="albumList" >
+                    {albums.slice(0, 8).map((album) => (
+                        <SingleCards
+                            key={album.id}
+                            photoPath={album.photoPath}
+                            album={album.album}
+                            band={album.band}
+                            price={album.price}
+                        />
+                    ))}
+                </div>
+            </div>
+
+            <Footer />
         </>
     );
 }
